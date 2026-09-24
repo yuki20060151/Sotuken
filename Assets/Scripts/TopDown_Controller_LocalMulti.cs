97dfa62f70f6b270216a944987ad82956fd71e7c
エビしゅりんぷ <104607268+yuki20060151@users.noreply.github.com>
2026-09-24 21:24:12 +0900

    TopDownConの追加＆編集。 プレイヤーのモデル設定

--- Assets/Scripts/TopDown_Controller_LocalMulti.cs
+++ Assets/Scripts/TopDown_Controller_LocalMulti.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,34 +6,29 @@ using UnityEngine.InputSystem;
 public class TopDown_Controller_LocalMulti : MonoBehaviour
 {
     #region インスペクター設定
-    [Header("移動パラメータ")]
+    [Header("移動設定")]
     [SerializeField] float walkSpeed = 3;
-    [SerializeField] float sprintSpeed = 6;
-    [SerializeField] float crouchSpeed = 1.5f;
-    [SerializeField] float rotationSpeed = 10;
-    [SerializeField] float terminalVelocity = 50;
+    [SerializeField] float bodyRotationSpeed = 10;
+    [Header("重力設定")]
     [SerializeField] float jumpHeight = 2;
     [SerializeField] float gravity = 9.81f;
-
-    [Header("立てないオブジェクトのレイヤー")]
-    [SerializeField] LayerMask cannotStandLayer = 1;
+    [SerializeField] float terminalVelocity = 50;
 
     [Header("地面判定")]
     [SerializeField] LayerMask layer;
     #endregion
 
     #region パブリック変数
-    [NonSerialized] public GameObject cam;
+
     #endregion
 
     #region プライベート変数
     PlayerInput input;
-    InputAction move, crouch, jump, sprint;
+    InputAction move, jump, attack;
     CharacterController cc;
+    GameObject cam;
     Vector3 moveDir, lookDir;
-    bool isCrouch;
     float currentMoveSpeed;
-    float playerStandHeight, playerStandCenter; //キャラクターコントローラーの高さと中心
     float verticalVelocity; //プレイヤーの縦移動速度
     //Animator anim, camAnim;
     #endregion
@@ -44,14 +38,11 @@ public class TopDown_Controller_LocalMulti : MonoBehaviour
         input = GetComponent<PlayerInput>();
         #region 入力取得
         move = input.actions["Move"];
-        crouch = input.actions["Crouch"];
-        sprint = input.actions["Sprint"];
         jump = input.actions["Jump"];
+        attack = input.actions["Attack"];
         #endregion
 
         cc = GetComponent<CharacterController>();
-        playerStandCenter = cc.center.y;
-        playerStandHeight = cc.height;
         currentMoveSpeed = walkSpeed;
 
         cam = Camera.main.gameObject;
@@ -66,46 +57,22 @@ public class TopDown_Controller_LocalMulti : MonoBehaviour
     #region 入力システム
     void OnEnable()
     {
-        crouch.performed += OnCrouch;
-        sprint.performed += OnSprint;
         jump.performed += OnJump;
+        attack.performed += OnAttack;
     }
     void OnDisable()
     {
-        crouch.performed -= OnCrouch;
-        sprint.performed -= OnSprint;
         jump.performed -= OnJump;
-    }
-    void OnCrouch(InputAction.CallbackContext context)
-    {
-        if (isCrouch) //座ってる状態だったら立てるか確認
-        {
-            var canStand = !Physics.CheckSphere
-            (
-                transform.position + new Vector3(0, playerStandHeight - cc.radius, 0),
-                cc.radius,
-                cannotStandLayer,
-                QueryTriggerInteraction.Ignore
-            );
-            if (!canStand) return; //無理なら返す
-        }
-
-        isCrouch = !isCrouch;
-        cc.height = isCrouch ? playerStandHeight * 0.6f : playerStandHeight;
-        float y = isCrouch ? playerStandCenter * 0.6f : playerStandCenter;
-        cc.center = new Vector3(0, y, 0);
-        currentMoveSpeed = isCrouch ? crouchSpeed : walkSpeed;
-    }
-    void OnSprint(InputAction.CallbackContext context)
-    {
-        if (isCrouch) return;
-        var isSprint = sprint.IsPressed(); //呼んだ時に押してたらtrue,離してたらfalse(押した瞬間だけを取るなら.triggered)
-        currentMoveSpeed = isSprint ? sprintSpeed : walkSpeed;
+        attack.performed -= OnAttack;
     }
     void OnJump(InputAction.CallbackContext context)
     {
         if (!Physics.CheckSphere(transform.position + new Vector3(0, cc.radius - 0.01f, 0), cc.radius, layer, QueryTriggerInteraction.Ignore)) return;
         verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * -gravity);
+    }
+    void OnAttack(InputAction.CallbackContext context)
+    {
+        print("アタック!!");
     }
     #endregion
 
@@ -124,17 +91,15 @@ public class TopDown_Controller_LocalMulti : MonoBehaviour
         Vector3 camRight = cam.transform.right; camRight.y = 0f; camRight.Normalize();
         Vector2 input_Move = move.ReadValue<Vector2>();
         moveDir = camForward * input_Move.y + camRight * input_Move.x;
-        if(moveDir != Vector3.zero)
-        {
+        if (moveDir != Vector3.zero)
             lookDir = moveDir;
-        }
         cc.Move(moveDir * currentMoveSpeed * Time.deltaTime);
     }
     void RotationBody()
     {
         if (lookDir == Vector3.zero) return;
         Quaternion targetRotation = Quaternion.LookRotation(lookDir);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, bodyRotationSpeed * Time.deltaTime);
     }
     void SetGravity()
     {
@@ -173,16 +138,6 @@ public class TopDown_Controller_LocalMulti : MonoBehaviour
         if (cc == null) return;
         Gizmos.color = Physics.CheckSphere(transform.position + new Vector3(0, cc.radius - 0.01f, 0), cc.radius, layer, QueryTriggerInteraction.Ignore) ? Color.green : Color.red;
         Gizmos.DrawWireSphere(transform.position + new Vector3(0, cc.radius, 0), cc.radius);
-
-        if (!isCrouch) return;
-        Gizmos.color = Physics.CheckSphere
-        (
-            transform.position + new Vector3(0, playerStandHeight - cc.radius, 0),
-            cc.radius,
-            cannotStandLayer,
-            QueryTriggerInteraction.Ignore
-        ) ? Color.red : Color.green;
-        Gizmos.DrawWireSphere(transform.position + new Vector3(0, playerStandHeight - cc.radius, 0), cc.radius);
     }
 }
 
